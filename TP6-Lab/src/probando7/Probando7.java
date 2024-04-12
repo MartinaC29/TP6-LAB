815c44cb05c4c1cf6cfd8491259a682a4803b442
PabloMaidana <pablomaid1@gmail.com>
2024-04-12 17:08:09 -0300

    projecto

---
+++ TP6-Lab/src/probando7/Probando7.java
@@ -0,0 +1,11 @@
+
+package probando7;
+
+
+public class Probando7 {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ TP6-Lab/src/probando7/Ventanas/Ventana.java
@@ -0,0 +1,264 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+
+package probando7.Ventanas;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
+
+
+public class Ventana extends javax.swing.JFrame {
+
+    
+    public Ventana() {
+        initComponents();
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jcbRubro = new javax.swing.JComboBox<>();
+        jtfStock = new javax.swing.JTextField();
+        jtfPrecio = new javax.swing.JTextField();
+        jtfDescripcion = new javax.swing.JTextField();
+        jtfCodigo = new javax.swing.JTextField();
+        jbGuardar = new javax.swing.JButton();
+        jbEliminar = new javax.swing.JButton();
+        jlErrorCodigo = new javax.swing.JLabel();
+        jlErrorDescripcion = new javax.swing.JLabel();
+        jlErrorStock = new javax.swing.JLabel();
+        jlErrorPrecio = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Gestión de productos");
+
+        jLabel2.setText("Código:");
+
+        jLabel3.setText("Descripción:");
+
+        jLabel4.setText("Precio: ");
+
+        jLabel5.setText("Rubro:");
+
+        jLabel6.setText("Stock:");
+
+        jcbRubro.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Comestible", "Perfumeria", "Limpieza", " " }));
+
+        jbGuardar.setText("Guardar");
+        jbGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbGuardarActionPerformed(evt);
+            }
+        });
+
+        jbEliminar.setText("Eliminar");
+
+        jlErrorCodigo.setForeground(new java.awt.Color(255, 0, 0));
+        jlErrorCodigo.setText("Dato erroneo");
+
+        jlErrorDescripcion.setForeground(new java.awt.Color(255, 0, 0));
+        jlErrorDescripcion.setText("Dato erroneo");
+
+        jlErrorStock.setForeground(new java.awt.Color(255, 0, 0));
+        jlErrorStock.setText("Dato erroneo");
+
+        jlErrorPrecio.setForeground(new java.awt.Color(255, 0, 0));
+        jlErrorPrecio.setText("Dato erroneo");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(137, 137, 137)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(21, 21, 21)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel6))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jtfPrecio)
+                            .addComponent(jtfDescripcion)
+                            .addComponent(jtfCodigo)
+                            .addComponent(jtfStock)
+                            .addComponent(jcbRubro, 0, 137, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jlErrorDescripcion)
+                            .addComponent(jlErrorStock)
+                            .addComponent(jlErrorPrecio)
+                            .addComponent(jlErrorCodigo)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(95, 95, 95)
+                        .addComponent(jbGuardar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jbEliminar)))
+                .addContainerGap(72, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(39, 39, 39)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(jtfCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jlErrorCodigo))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(jtfDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jlErrorDescripcion))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(jtfPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jlErrorPrecio))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel5)
+                            .addComponent(jcbRubro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel6))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jtfStock, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jlErrorStock)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jbGuardar)
+                    .addComponent(jbEliminar))
+                .addGap(15, 15, 15))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jbGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbGuardarActionPerformed
+        
+        //Verificar que el usuario cargue bien los datos 
+        
+        Pattern p = Pattern.compile("\\d{1,10}");
+        Matcher m = p.matcher(jtfCodigo.getText()); 
+
+        boolean codigoInvalido;
+        boolean descripcionInvalido;
+        boolean precioInvalido; 
+        boolean stockInvalido; 
+        jlErrorCodigo.setVisible(false);
+        jlErrorDescripcion.setVisible(false);
+        jlErrorPrecio.setVisible(false);
+        jlErrorStock.setVisible(false);
+        
+        
+        if (!m.matches()) {
+            codigoInvalido = true; 
+        }
+        
+        Pattern b = Pattern.compile("^[a-zA-Z]+$");
+        Matcher s = b.matcher(jtfDescripcion.getText());
+
+        if (!s.matches()) {
+            descripcionInvalido = true; 
+        }
+        
+         Pattern t = Pattern.compile("^[-+]?[0-9]*\\\\.?[0-9]+$");
+        Matcher l = t.matcher(jtfPrecio.getText());
+        
+        if (!l.matches()) {
+            precioInvalido = true; 
+        }
+        
+        Pattern a = Pattern.compile("^[+]?\\\\d+$");
+        Matcher o = a.matcher(jtfStock.getText());
+        
+        if (!o.matches()) {
+            stockInvalido = true; 
+        }
+        
+        
+        //Carga de datos 
+        String codigo = jtfCodigo.getText();
+        String descripcion = jtfDescripcion.getText();
+        double precio = Double.parseDouble(jtfPrecio.getText());
+        String rubro = jcbRubro.getSelectedItem().toString();
+        int stock = Integer.parseInt(jtfStock.getText()); 
+        
+        
+    }//GEN-LAST:event_jbGuardarActionPerformed
+
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Ventana().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JButton jbEliminar;
+    private javax.swing.JButton jbGuardar;
+    private javax.swing.JComboBox<String> jcbRubro;
+    private javax.swing.JLabel jlErrorCodigo;
+    private javax.swing.JLabel jlErrorDescripcion;
+    private javax.swing.JLabel jlErrorPrecio;
+    private javax.swing.JLabel jlErrorStock;
+    private javax.swing.JTextField jtfCodigo;
+    private javax.swing.JTextField jtfDescripcion;
+    private javax.swing.JTextField jtfPrecio;
+    private javax.swing.JTextField jtfStock;
+    // End of variables declaration//GEN-END:variables
+
+}
