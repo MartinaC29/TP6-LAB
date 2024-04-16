6245ba8dc866814b28523f6f35408e7ea30d922c
PabloMaidana <pablomaid1@gmail.com>
2024-04-15 23:37:48 -0300

    ListadoPrecio ExceptionFix

--- TP6-Lab/src/TP6_LAB/Ventanas/ListadoPrecio.java
+++ TP6-Lab/src/TP6_LAB/Ventanas/ListadoPrecio.java
@@ -2,6 +2,8 @@ package TP6_LAB.Ventanas;
 
 import TP6_LAB.entidades.Producto;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
@@ -10,12 +12,14 @@ public class ListadoPrecio extends javax.swing.JInternalFrame {
     //Atributos y colecciones
     private TreeSet<Producto> productos;
     private DefaultTableModel modelo; 
+    private boolean numInvalido1,numInvalido2;
     
     //Constructor 
     public ListadoPrecio(TreeSet<Producto> productos) {
         initComponents();
         this.modelo = (DefaultTableModel) jtTabla.getModel();
-        this.productos = productos; 
+        this.productos = productos;
+        jlNumInvalido.setVisible(false);
     }
 
   
@@ -30,6 +34,9 @@ public class ListadoPrecio extends javax.swing.JInternalFrame {
         jLabel3 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jtTabla = new javax.swing.JTable();
+        jlNumInvalido = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
 
         setClosable(true);
         setMaximizable(true);
@@ -68,29 +75,42 @@ public class ListadoPrecio extends javax.swing.JInternalFrame {
         ));
         jScrollPane1.setViewportView(jtTabla);
 
+        jlNumInvalido.setForeground(new java.awt.Color(255, 51, 51));
+        jlNumInvalido.setText("Nro. invalido");
+
+        jLabel4.setText("Min:");
+
+        jLabel5.setText("Max:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(56, 56, 56)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 396, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(53, 53, 53)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel4))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jtfPrecio1, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel3)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel5)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jtfPrecio2, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(jtfPrecio2, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jlNumInvalido))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(8, 8, 8)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 396, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(18, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -98,12 +118,16 @@ public class ListadoPrecio extends javax.swing.JInternalFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addGap(28, 28, 28)
+                .addGap(3, 3, 3)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
                     .addComponent(jtfPrecio1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jtfPrecio2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
+                    .addComponent(jLabel3)
+                    .addComponent(jlNumInvalido)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(14, Short.MAX_VALUE))
@@ -120,13 +144,30 @@ public class ListadoPrecio extends javax.swing.JInternalFrame {
         Double precioMax = 9999999.0; 
 
         if (!jtfPrecio1.getText().isEmpty() ) {
-       precioMin = Double.parseDouble(jtfPrecio1.getText());
+            if (validarDouble(jtfPrecio1.getText())) {
+                precioMin = Double.parseDouble(jtfPrecio1.getText());
+                numInvalido1 = false;
+            }else{
+                jlNumInvalido.setVisible(true);
+                numInvalido1 = true;
+            }    
         }
 
-         if (!jtfPrecio2.getText().isEmpty() ) {
-       precioMax = Double.parseDouble(jtfPrecio2.getText());
+        if (!jtfPrecio2.getText().isEmpty() ) {
+            if (validarDouble(jtfPrecio2.getText())) {
+                precioMax = Double.parseDouble(jtfPrecio2.getText());
+                numInvalido2 = false;
+            }else{
+                jlNumInvalido.setVisible(true);
+                numInvalido2 = true;
+            }
         }
         
+        if (!numInvalido1 && !numInvalido2) {
+            jlNumInvalido.setVisible(false);
+        }
+        
+        if (!numInvalido1 && !numInvalido2) {
             for (Producto prod : productos) {
                 
                 if (precioMin <= prod.getPrecio() && precioMax == 9999999.0) {  //Si el usuario rellenó el campo de precio mínimo
@@ -149,7 +190,7 @@ public class ListadoPrecio extends javax.swing.JInternalFrame {
                     }
                 }
             }
-        
+        }   
     }
     
     private void borrarFilas(){
@@ -158,6 +199,12 @@ public class ListadoPrecio extends javax.swing.JInternalFrame {
              modelo.removeRow(f);
          }
      }
+    
+    private boolean validarDouble(String s){
+        Pattern p = Pattern.compile("^[-+]?[0-9]*\\.?[0-9]+$");
+        Matcher m = p.matcher(s);
+        return m.matches();
+    }
     
     //métodos key release 
     private void jtfPrecio1KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtfPrecio1KeyReleased
@@ -177,7 +224,10 @@ public class ListadoPrecio extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel jlNumInvalido;
     private javax.swing.JTable jtTabla;
     private javax.swing.JTextField jtfPrecio1;
     private javax.swing.JTextField jtfPrecio2;
